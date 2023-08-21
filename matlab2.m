de88c70bd193b2bd10b27ea2bbb89302b713b014
vaibhav02856 <141218198+vaibhav02856@users.noreply.github.com>
2023-08-22 02:22:36 +0530

    Add files via upload

---
+++ matlab2.m
@@ -0,0 +1,10 @@
+rating=randi([0,10],[1,1])
+if (rating<3)
+    disp("poor")
+elseif(rating>3)&&(rating<=5)
+    disp ("avg")
+elseif(rating>5)&&(rating<=7)
+    disp("good")
+elseif(rating>7)&&(rating<=10)
+    disp("excellent")
+end
