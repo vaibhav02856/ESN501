c3dbe619cadf7ae4d12bad07d6f5239f012cced7
vaibhav02856 <141218198+vaibhav02856@users.noreply.github.com>
2023-08-25 22:32:32 +0530

    Add files via upload

---
+++ assignment1.m
@@ -0,0 +1,24 @@
+clear all
+close all
+a=45;
+b=80;
+c=90;
+d=23;
+e=input("write value e\n" );
+f=input("write value f\n");
+g=input("write value g\n");
+h=input("write value h\n");
+x=a+e;
+y=b-f;
+z=c*g;
+w=d/h;
+fprintf("%d+ %d= %d\n",a,e,x);
+fprintf("%d-%d=%d\n ",b,f,y);
+fprintf("%d*%d=%d\n",c,g,z);
+fprintf("%d/%d=d\n",d,h,w);
+
+
+
+
+
+
